f3fa75a03808ed34725b4942cf6fd5a6996f956a
Giancarlos Salas <me@giansalex.dev>
2022-09-18 19:55:52 -0500

    Update errors

--- contracts/swap/src/error.rs
+++ contracts/swap/src/error.rs
@@ -1,6 +1,10 @@
 use cosmwasm_std::StdError;
 use thiserror::Error;
 
+/// Never is a placeholder to ensure we don't return any errors
+#[derive(Error, Debug)]
+pub enum Never {}
+
 #[derive(Error, Debug)]
 pub enum ContractError {
     #[error("{0}")]
@@ -9,4 +13,16 @@ pub enum ContractError {
     #[error("Unauthorized")]
     Unauthorized {},
     // Add any other custom errors you like here.
+
+    #[error("Channel not found")]
+    ChannelNotFound {},
+
+    #[error("Swap order not found")]
+    OrderNotFound {},
+
+    #[error("Only supports channel with ibc version ics20-1, got {version}")]
+    InvalidIbcVersion { version: String },
+
+    #[error("Only supports unordered channel")]
+    OnlyOrderedChannel {},
 }
